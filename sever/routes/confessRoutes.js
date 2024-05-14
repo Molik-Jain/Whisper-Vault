d8ad115aef77bbe3c67c7342f8606026d8cf820f
Molik Jain <105535121+Molik-Jain@users.noreply.github.com>
2024-05-14 14:44:44 +0530

    Update confessRoutes.js

--- sever/routes/confessRoutes.js
+++ sever/routes/confessRoutes.js
@@ -10,7 +10,7 @@ const { isAuthenticated } = require('../config/auth');
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'https://whisper-vault.vercel.app/'
     })
 )
 
